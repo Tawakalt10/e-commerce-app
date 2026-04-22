a79c8ddc791cbb3533b7168e015a3e7c712a725e
Tawakalt10 <raheemtawa@gmail.comm>
2026-04-22 23:29:06 +0100

    new

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -17,6 +17,6 @@ export default  function ProductCard ({product}) {
                                 <button className="btn btn-primary"> Add to Cart</button>
                             </div>
                         </div>
-                    </div>
+                      </div>
     )
 }
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,5 +1,33 @@
 export default function Auth() {
-    return <div>
-        Auth Page
+    return  ( 
+    <div className="page"> 
+        <div className="container">
+            <div className="auth-container">
+                <h1 className="page-title">
+                    Signup
+                </h1>
+           <form className="auth-form">
+             <div className="form-group">
+             <label className="form-label" htmlFor="email"> Email</label>
+             <input className="form-input" type="email" id="email"/>
+             </div>
+
+                <div className="form-group">
+             <label className="form-label" htmlFor="password"> Password</label>
+             <input className="form-input" type="passwords" is="password"/>
+             </div>
+
+             <button type="submit" className="btn btn-primary btn-large"> Sign Up</button>
+           </form>
+
+           <div className="auth-switch">
+             <p>
+                Already have an account <span className="auth-link"> Login</span>
+             </p>
+           </div>
+            </div>
+
+        </div>
     </div>
+    )
 }
